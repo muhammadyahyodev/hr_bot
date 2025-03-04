7da2099d75feb039fb10b3279db5f9614893fc83
yakhyo <yakhyomuhammad1@gmail.com>
2025-03-04 10:04:49 +0500

    Move AppDataSource initializer

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -10,3 +10,12 @@ export const AppDataSource = new DataSource({
     synchronize: false,
     logging: true,
 } as DataSourceOptions);
+
+AppDataSource
+    .initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization:", err)
+    })
