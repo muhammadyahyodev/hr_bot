09f1e31c49ee3adc39d50e92b80fe0bf7da0e0c4
yakhyo <yakhyomuhammad1@gmail.com>
2025-03-04 10:23:10 +0500

    Update

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -1,6 +1,4 @@
 import { Bot } from "grammy";
-import dotenv from 'dotenv';
-dotenv.config();
 
 const bot = new Bot(process.env.BOT_TOKEN as string); 
 
--- src/core/run.ts
+++ src/core/run.ts
@@ -1,15 +1,13 @@
 import { AppDataSource } from '../database/data-source';
 import { bot } from './bot';
 
-if (!AppDataSource.isInitialized) {
-    AppDataSource
-        .initialize()
-        .then(() => {
-            console.log("Data Source has been initialized!")
-        })
-        .catch((err) => {
-            console.error("Error during Data Source initialization:", err)
-        })
-}
+AppDataSource
+    .initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization:", err)
+    })
 
 bot.start();
